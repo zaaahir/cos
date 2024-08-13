0404bdef45525bf3fef175ccc5acfcde4d2604b8
Zaahir Ali <104582172+zaaahir@users.noreply.github.com>
2024-08-13 16:07:17 +0100

    Update dispatcher.h

--- include/kernel/event/dispatcher.h
+++ include/kernel/event/dispatcher.h
@@ -30,6 +30,7 @@ namespace Events {
         void* param = nullptr;
         void* process = nullptr;
         Common::DoublyLinkedList<EventMessage> queue;
+        Common::DoublyLinkedList<Common::DoublyLinkedList<EventDescriptorItem>*> listenerListElements;
         Common::DoublyLinkedList<Common::DoublyLinkedList<EventDescriptorItem>*> waitListElements;
     };
 
@@ -37,7 +38,9 @@ namespace Events {
     {
         char str[180];
         friend bool operator==(const FixedLenStringHashContainer& lhs, const FixedLenStringHashContainer& rhs)
-// not sure how i'm going to implement
+        {
+            return !strcmp(lhs.str, rhs.str);
+        }
     };
 
     class EventDispatcher
@@ -51,11 +54,29 @@ namespace Events {
         EventDispatcher(EventDispatcher const&) = delete;
         EventDispatcher& operator=(EventDispatcher const&) = delete;
 
+        // Open an event descriptor to receive event messages for the event.
+        static uint64_t register_event_listener(uint64_t tid, char* event, void* param);
+        // Close the event descriptor.
+        static uint64_t deregister_event_listener(uint64_t tid, uint64_t ped);
+        // Read from the event descriptor's queue of messages.
+        static uint64_t read_from_event_queue(uint64_t tid, uint64_t ped);
+        // Block the process until an event message is received.
+        static void block_event_listen(uint64_t tid, uint64_t ed);
+        
+        // Register as an event sender with an event name.
+        void register_event_sender(char* event, EventSender* sender);
+        // Send an event message to all listeners.
+        void dispatch_event(EventMessage event, Common::DoublyLinkedList<EventDescriptorItem>* processes);
+        void add_process_to_listener_event_list(uint64_t ped, uint64_t tid, Common::DoublyLinkedList<EventDescriptorItem>* list);
+        void add_process_to_wait_event_list(uint64_t ped, uint64_t tid, Common::DoublyLinkedList<EventDescriptorItem>* list);
+        // Wake all processes in the list.
+        void wake_processes(Common::DoublyLinkedList<EventDescriptorItem>* list);
     private:
         EventDispatcher() {}
         Common::Hashmap<FixedLenStringHashContainer, EventSender*> m_eventSenderHashmap;
         void dispatch_event(EventMessage event, EventDescriptorItem processed);
         void add_process_to_event_list(EventDescriptorItem item, Common::DoublyLinkedList<EventDescriptorItem>* list);
+        void remove_process_from_listener_lists(uint64_t ped, uint64_t tid);
         void remove_process_from_wait_lists(uint64_t ped, uint64_t tid);
     };
 }
