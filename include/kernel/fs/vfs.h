1f8762fbbe49ff31b890b1675cd54ced7b44522b
Zaahir Ali <104582172+zaaahir@users.noreply.github.com>
2024-08-13 12:55:12 +0100

    Update vfs.h

--- include/kernel/fs/vfs.h
+++ include/kernel/fs/vfs.h
@@ -40,6 +40,7 @@ namespace Filesystem
         
         uint64_t creationTime = 0;
         uint64_t modificationTime = 0;
+        uint64_t accessedTime = 0;
 
         VFSDriver* driver;
     };
@@ -54,6 +55,52 @@ namespace Filesystem
         uint64_t fd;
         uint64_t offset;
         Filesystem::VirtualFilesystemNode* node;
+    };
+
+    class VirtualFilesystemManager;
+
+    // Concrete filesystems are mounted in the virtual filesystem.
+    // The virtual filesystem calls the concrete filesystem's driver for file operations.
+    class RealFilesystem
+    {
+        friend class VirtualFilesystemManager;
+    protected:
+        int m_mountNumber;
+        VFSDriver* m_driver;
+        int m_rootVirtualNodeNumber;
+        Common::Hashmap<uint64_t, VirtualFilesystemNode> m_virtualNodeHashmap;
+    public:
+        RealFilesystem(VirtualFilesystemNode rootNode, VFSDriver* driver) : m_rootVirtualNodeNumber(rootNode.inodeNum), m_driver(driver)
+        {
+            m_virtualNodeHashmap.insert(rootNode.inodeNum, rootNode);
+        }
+    };
+
+    class VirtualFilesystemManager
+    {
+    public:
+        static VirtualFilesystemManager& instance()
+        {
+            static VirtualFilesystemManager instance;
+            return instance;
+        }
+        VirtualFilesystemManager(VirtualFilesystemManager const&) = delete;
+        VirtualFilesystemManager& operator=(VirtualFilesystemManager const&) = delete;
+        int register_filesystem(RealFilesystem filesystem); 
+        void unregister_filesystem(int mountNum);
+        
+        VirtualFilesystemNode* open_file(char* filename, uint64_t flags);
+        // Read count bytes from offset into buf.
+        uint64_t read(VirtualFilesystemNode* vnode, uint64_t offset, uint64_t count, void *buf);
+        // Write count bytes from offset into buf.
+        uint64_t write(VirtualFilesystemNode* vnode, uint64_t offset, uint64_t count, void *buf);
+        // Get item in directory node.
+        VirtualFilesystemNode* finddir(VirtualFilesystemNode* vnode, char* filename);
+        uint64_t filelen(VirtualFilesystemNode* vnode);
+    private:
+        VirtualFilesystemManager() { }
+        int m_fsAllocNum = 0;
+        Common::DoublyLinkedList<RealFilesystem> m_mountedFilesystems;
     };
 }
 
