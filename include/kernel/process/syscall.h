9aa4ddfa840e9b0ae176f8ef2c7c605a913d03c6
Zaahir Ali <104582172+zaaahir@users.noreply.github.com>
2024-08-14 16:27:40 +0100

    Update syscall.h

--- include/kernel/process/syscall.h
+++ include/kernel/process/syscall.h
@@ -16,6 +16,67 @@
 #define SYSCALL_SLEEP_FOR 12
 #define SYSCALL_GETTID 13
 
-// placeholder
+uint64_t syscall(uint64_t num)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num));
+    return ret;
+}
+
+uint64_t syscall(uint64_t num, uint64_t arg1)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n" "mov %2, %%rdi\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num), "r" (arg1): "rdi");
+    return ret;
+}
+
+uint64_t syscall(uint64_t num, uint64_t arg1, uint64_t arg2)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n" "mov %2, %%rdi\n" "mov %3, %%rsi\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num), "r" (arg1), "r" (arg2): "rdi", "rsi");
+    return ret;
+}
+
+uint64_t syscall(uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n" "mov %2, %%rdi\n" "mov %3, %%rsi\n" "mov %4, %%rdx\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num), "r" (arg1), "r" (arg2), "r" (arg3) : "rdi", "rsi", "rdx");
+    return ret;
+}
+
+uint64_t syscall(uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n" "mov %2, %%rdi\n" "mov %3, %%rsi\n" "mov %4, %%rdx\n" "mov %5, %%r10\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num), "r" (arg1), "r" (arg2), "r" (arg3), "r" (arg4) : "rdi", "rsi", "rdx", "r10");
+    return ret;
+}
+
+uint64_t syscall(uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n" "mov %2, %%rdi\n" "mov %3, %%rsi\n" "mov %4, %%rdx\n" "mov %5, %%r10\n" "mov %6, %%r8\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num), "r" (arg1), "r" (arg2), "r" (arg3), "r" (arg4), "r" (arg5): "rdi", "rsi", "rdx", "r10", "r8");
+    return ret;
+}
+
+uint64_t syscall(uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6)
+{
+    uint64_t ret;
+    asm volatile("mov %1, %%rax\n" "mov %2, %%rdi\n" "mov %3, %%rsi\n" "mov %4, %%rdx\n" "mov %5, %%r10\n" "mov %6, %%r8\n" "mov %7, %%r9\n"
+                    "syscall\n"
+                    "mov %%rax, %0\n" : "=r"(ret) : "r" (num), "r" (arg1), "r" (arg2), "r" (arg3), "r" (arg4), "r" (arg5), "r" (arg6): "rdi", "rsi", "rdx", "r10", "r8", "r9");
+    return ret;
+}
 
 #endif
