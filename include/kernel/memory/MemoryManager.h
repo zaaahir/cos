87882a5cfe8e12efecdd60c0c557592f66ecc367
Zaahir Ali <104582172+zaaahir@users.noreply.github.com>
2024-08-15 11:52:52 +0100

    Update MemoryManager.h

--- include/kernel/memory/MemoryManager.h
+++ include/kernel/memory/MemoryManager.h
@@ -74,3 +74,82 @@ namespace Memory {
         VirtualMemoryUnmapRequest(VirtualAddress virtualAddress) : virtualAddress(virtualAddress) {}
         VirtualMemoryUnmapRequest(VirtualMemoryFreeRequest request) : virtualAddress(request.virtualAddress) {}
     };
+
+    class GenericEntry
+    {
+        uint64_t m_entry = 0;
+    public:
+        enum Flag : uint64_t
+        {
+            PRESENT = 0x1,
+            WRITEABLE = 0x2,
+            USER_ACCESS = 0x4,
+            PAGE_SIZE = 0x80
+        };
+        bool get_flag(Flag flag) { return m_entry & flag; }
+        void set_flag(Flag flag) { m_entry |= flag; }
+        void clear_flag(Flag flag) { m_entry & (~flag); }
+        void set_frame(PhysicalAddress frame)
+        {
+            m_entry = reinterpret_cast<uint64_t>(frame.get()) | (m_entry & 0xFFF);
+        }
+
+        void clear() { m_entry = 0; }
+
+        bool is_present() { return get_flag(PRESENT); }
+
+        PhysicalAddress get_frame()
+        {
+            return PhysicalAddress(m_entry & ~0xFFF);
+        }
+
+        void set_access_flags(VirtualMemoryMapRequest request);
+
+        void prepare_table_for_entry(VirtualMemoryMapRequest request);
+
+        void prepare_page_for_entry(VirtualMemoryMapRequest request);
+
+        void free_table();
+    };
+
+    class GenericTable
+    {
+    public:
+        bool is_table_empty()
+        {
+            uint64_t* p = (uint64_t*)m_entries;
+            uint64_t sz = ENTRIES_PER_TABLE;
+            while (sz--)
+                if (*p++ != 0) { return false; }
+            return true;
+        }
+    protected:
+        static constexpr int ENTRIES_PER_TABLE = 512;
+        GenericEntry m_entries[ENTRIES_PER_TABLE] = {};
+    };
+
+    class PML4Table : public GenericTable
+    {
+    public:
+        void request_virtual_map(VirtualMemoryMapRequest request);
+        void request_virtual_unmap(VirtualMemoryUnmapRequest request);
+        PhysicalAddress get_physical_address(VirtualAddress virtualAddress);
+    private:
+        GenericEntry* get_entry(VirtualAddress virtualAddress)
+        {
+            return &m_entries[(reinterpret_cast<uint64_t>(virtualAddress.get()) >> 39) & 0x1FF];
+        }
+    } __attribute__((packed));
+
+    class PageDirectoryPointerTable : public GenericTable
+    {
+    public:
+        void request_virtual_map(VirtualMemoryMapRequest request);
+        bool request_virtual_unmap(VirtualMemoryUnmapRequest request);
+        PhysicalAddress get_physical_address(VirtualAddress virtualAddress);
+    private:
+        GenericEntry* get_entry(VirtualAddress virtualAddress)
+        {
+            return &m_entries[(reinterpret_cast<uint64_t>(virtualAddress.get()) >> 30) & 0x1FF];
+        }
+    } __attribute__((packed));
