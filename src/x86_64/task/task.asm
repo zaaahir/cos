40987cb94f37f06fc4eb0968047d29f8e733bd54
Zaahir Ali <104582172+zaaahir@users.noreply.github.com>
2024-08-18 19:06:41 +0100

    Update task.asm

--- src/x86_64/task/task.asm
+++ src/x86_64/task/task.asm
@@ -72,3 +72,64 @@ isr_def_32:
     call _ZN4Task11TaskManager14get_safe_stackEv
     mov rsp, rax
     jmp _ZN4Task11TaskManager8pit_tickEv
+
+block_task_and_refresh:
+    cli
+    ; Pop return address off stack and into rax.
+    pop rax
+    ; Place a fake interrupt stack on the task's kernel stack so it can be returned to.
+    mov rcx, rsp
+    push (2*8)
+    push rcx
+    push 0x202
+    push (1*8)
+    push rax
+    pushaq
+    mov rsi, rdi
+    mov rdi, rsp
+    ; Save the current kernel stack and switch to the safe stack.
+    call _ZN4Task11TaskManager20save_kstack_with_tidEPvy
+    call _ZN4Task11TaskManager14get_safe_stackEv
+    mov rsp, rax
+    jmp _ZN4Task11TaskManager7refreshEv
+
+return_to_task:
+    ; Switch to new page table for the new process if needed.
+    mov rcx, cr3
+    cmp rdi, rcx
+    je vaddrspace_changed
+    mov cr3, rdi
+vaddrspace_changed:
+    call _ZN4Task11TaskManager17get_current_stackEv
+    mov rsp, rax
+    jmp end_of_pit_reply
+
+call_pit_tick:
+    call _ZN4Task11TaskManager8pit_tickEv
+    popaq
+    iretq
+
+pit_interrupt_end:
+    mov al, 0x20
+    out 0x20, al
+end_of_pit_reply:
+    popaq
+    iretq
+
+send_pit_eoi:
+    mov al, 0x20
+    out 0x20, al
+    ret
+
+start_kernel_task_iretq:
+    ; Kernel data selector.
+    push (2*8)
+    ; Target stack pointer.
+    push rsi 
+    ; Enable interrupts in RFLAGS.
+    push 0x202
+    ; Kernel code selector.
+    push (1*8)
+    ; Target instruction pointer.
+    push rdi 
+    iretq
